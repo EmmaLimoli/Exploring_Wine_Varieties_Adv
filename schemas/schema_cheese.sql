adbbe40a9dbad075a683f46a0286f29818ac453e
Emma Limoli <EmmaLimoli024@gmail.com>
2021-01-04 14:57:36 -0500

    started creating cheese database

---
+++ schemas/schema_cheese.sql
@@ -0,0 +1,11 @@
+create table blue_pair (
+	Variety varchar,
+	Wines varchar,
+	Beverages varchar,
+	"Fruits/Vegetables" varchar,
+	"Breads/Crackers" varchar,
+	"Condiments/Nuts" varchar,
+	"Meats/Fish" varchar	
+)
+
+SELECT * FROM blue_pair;
