8fcb959801009375195a9789187a0c8abe899292
Emma Limoli <EmmaLimoli024@gmail.com>
2021-01-13 15:31:39 -0500

    start cheese page

--- UPDATED_APP/static/js/cheese.js
+++ UPDATED_APP/static/js/cheese.js
@@ -2,7 +2,7 @@
 // create filter to pull out specific cheeses
 
 // create a filepath to data.csv using d3.csv use a then function
-d3.csv("../CSVS/cheese.csv").then(function(cheese) {
-    console.log(cheese);
-});
+d3.csv("./chees.csv").then(function(cheesedata, err) {
+    console.log(cheesedata);
+}); 
 
