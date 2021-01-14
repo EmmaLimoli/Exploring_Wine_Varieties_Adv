aadb6857b46b8357d3cab769eb293a927bc005cb
Emma Limoli <EmmaLimoli024@gmail.com>
2021-01-14 13:08:47 -0500

    working on importing csv

--- UPDATED_APP/static/js/cheese.js
+++ UPDATED_APP/static/js/cheese.js
@@ -2,7 +2,12 @@
 // create filter to pull out specific cheeses
 
 // create a filepath to data.csv using d3.csv use a then function
-d3.csv("./chees.csv").then(function(cheesedata, err) {
-    console.log(cheesedata);
-}); 
+d3.csv("../data/cheese_pract.csv").then(function(data, err) {
+    console.log(data);
+});
+
+
+
+
+
 
