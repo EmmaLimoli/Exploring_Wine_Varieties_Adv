98e17dac95e2fd8981189d326e667b72663834df
Emma Limoli <EmmaLimoli024@gmail.com>
2021-01-21 14:37:18 -0500

    working on filtering variety/type/flavor

--- UPDATED_APP/cheese.js
+++ UPDATED_APP/cheese.js
@@ -1,6 +1,8 @@
 // use csv to pull in cheese data
 // create filter to pull out specific cheeses
 
+var tbody = d3.select("tbody")
+
 // create a filepath to data.csv using d3.csv use a then function
 d3.csv("data/cheese_pract.csv").then(function(data, err) {
     console.log(data);
@@ -12,6 +14,19 @@ d3.csv("data/cheese_pract.csv").then(function(data, err) {
     console.log(key, value)
     })
     })
+});
+
+// filter by cheese variety, type, flavor, like ufo project
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    tbody.html("");
+    d3.event.preventDefault();
+    var inputElement = d3.select("#cheeseVariety");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+    console.log(tableData);
+    var filterData = data.filter(cheese => cheese.variety == inputValue);
+    console.log(filterData)
 });
 
 
